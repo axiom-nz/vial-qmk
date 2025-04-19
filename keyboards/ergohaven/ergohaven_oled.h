4ae98455d3412b455326149ef6104a50781604c0
Elliot Ruddock <elliot@ruddock.nz>
2025-04-20 11:41:58 +1200

    Migrate velvet3 to userspace

--- keyboards/ergohaven/ergohaven_oled.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void housekeeping_task_split_oled(void);
--- keyboards/ergohaven/ergohaven_rgb.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "quantum.h"
-
-void keyboard_post_init_rgb(void);
-void layer_state_set_rgb(layer_state_t state);
-void rgb_on(void);
-void rgb_off(void);
